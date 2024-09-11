8b98cf8bb2a1b0545f0f77027dbed6262134e813
therishitakerr <therishitakerr@gmail.com>
2024-09-11 15:45:04 +0530

    ..

--- ArrangeMusic.sh
+++ ArrangeMusic.sh
@@ -2,6 +2,10 @@
 
 currentDirectory="$1" #pwd if $1 is null
 
+if [[ -z $1 ]]; then
+    currentDirectory=$(pwd)
+fi
+
 # Process each media file in the current directory
 for file in "$currentDirectory"/*.mp3 "$currentDirectory"/*.wav "$currentDirectory"/*.flac "$currentDirectory"/*.aac "$currentDirectory"/*.ogg "$currentDirectory"/*.m4a "$currentDirectory"/*.wma "$currentDirectory"/*.opus; do
     if [[ -f "$file" ]]; then
